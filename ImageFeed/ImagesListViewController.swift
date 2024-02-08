8369e0606e2ec0f697fe64877afb27fafffe5f0f
Pleshakov-Alexandr <sanya.pleshakov.95@mail.ru>
2024-02-08 14:00:02 +0300

    Added conforming to UITableView protocols for ImagesList

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -9,15 +9,44 @@ import UIKit
 
 class ImagesListViewController: UIViewController {
     
+    //MARK: Properties
+    
+    
+    
     // MARK: Outlets
     
     @IBOutlet weak var ImagesTableView: UITableView!
+    
+    // MARK: Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // MARK: Functions
+    
+    
+    
+    // MARK: Actions
 
+}
 
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
 }
 
