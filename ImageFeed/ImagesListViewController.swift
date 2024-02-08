2ff7007054bbd03e7dbf1a80a13ad35b1b3183d3
Pleshakov-Alexandr <sanya.pleshakov.95@mail.ru>
2024-02-08 13:44:17 +0300

    Added the TableView for images

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ImagesListViewController.swift
 //  ImageFeed
 //
 //  Created by Александр Плешаков on 06.02.2024.
@@ -7,8 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ImagesListViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var ImagesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
