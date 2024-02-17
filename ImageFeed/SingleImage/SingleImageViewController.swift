d3c44cf3ac3bb4482c91ebd536fa8c0ecdf0c80e
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-02-17 19:13:49 +0300

    Added transition to full-screen image

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Александр Плешаков on 17.02.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet private weak var imageView: UIImageView!
+}
