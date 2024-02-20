b68b12203c98f4240f37edb5f643ca1d41f53207
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-02-20 23:40:40 +0300

    Added centering of image

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -17,16 +17,22 @@ final class SingleImageViewController: UIViewController {
         }
     }
     
+//    lazy var zoomingTap: UITapGestureRecognizer = {
+//        let zoomingTap = UITapGestureRecognizer(target: scrollView, action: #selector(handleZoomingTap))
+//        zoomingTap.numberOfTapsRequired = 2
+//        return zoomingTap
+//    }()
+    
     // MARK: Outlets
     
-    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet private weak var scrollView: UIScrollView!
     @IBOutlet private weak var backwardButton: UIButton!
     @IBOutlet private weak var imageView: UIImageView!
     
     // MARK: Life Cycle
     
     override func viewDidLoad() {
-         super.viewDidLoad()
+        super.viewDidLoad()
         
         imageView.image = image
         imageView.frame.size = image.size
@@ -34,15 +40,26 @@ final class SingleImageViewController: UIViewController {
         configurateFor(imageSize: image.size)
     }
     
-    func configurateFor(imageSize: CGSize) {
+    override func viewWillLayoutSubviews() {
+        centerImage()
+    }
+    
+    // MARK: Methods
+    
+    private func configurateFor(imageSize: CGSize) {
         scrollView.bounds.size = view.bounds.size
         scrollView.contentSize = imageSize
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
         
         setCurrentMaxAndMinZoomScale()
         scrollView.zoomScale = scrollView.minimumZoomScale
+        
+//        imageView.addGestureRecognizer(zoomingTap)
+//        imageView.isUserInteractionEnabled = true
     }
     
-    func setCurrentMaxAndMinZoomScale() {
+    private func setCurrentMaxAndMinZoomScale() {
         let boundsSize = scrollView.bounds.size
         let imageSize = imageView.bounds.size
         let xScale = boundsSize.width / imageSize.width
@@ -67,9 +84,28 @@ final class SingleImageViewController: UIViewController {
         scrollView.maximumZoomScale = maxScale
     }
     
+    private func centerImage() {
+        let boundsSize = scrollView.bounds.size
+        var frameToCenter = imageView.frame
+        
+        if frameToCenter.size.width < boundsSize.width {
+            frameToCenter.origin.x = (boundsSize.width - frameToCenter.size.width) / 2
+        } else {
+            frameToCenter.origin.x = 0
+        }
+        
+        if frameToCenter.size.height < boundsSize.height {
+            frameToCenter.origin.y = (boundsSize.height - frameToCenter.size.height) / 2
+        } else {
+            frameToCenter.origin.y = 0
+        }
+        
+        imageView.frame = frameToCenter
+    }
+    
     // MARK: Actions
     
-    @IBAction func buttonBackwardTapped() {
+    @IBAction private func buttonBackwardTapped() {
         dismiss(animated: true)
     }
 }
@@ -80,4 +116,9 @@ extension SingleImageViewController: UIScrollViewDelegate {
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         imageView
     }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        centerImage()
+    }
 }
+
