afca69f7479e34a247f56d0500ea407bb8de2494
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-03-10 22:46:27 +0300

    Interface remade in storyboard

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -7,44 +7,11 @@
 
 import UIKit
 
+
 final class AuthViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configure()
-    }
-    
-    private func configure() {
-        let authImage = UIImage(named: "authImage")
-        let imageView = UIImageView(image: authImage)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(imageView)
-        
-        let button = UIButton()
-        button.setTitle("Войти", for: .normal)
-        button.setTitleColor(UIColor(named: "YP Black"), for: .normal)
-        button.backgroundColor = .white
-        button.layer.cornerRadius = 16
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.addTarget(self, action: #selector(showWebView), for: .touchUpInside)
-        view.addSubview(button)
-        
-        NSLayoutConstraint.activate([
-            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            imageView.widthAnchor.constraint(equalToConstant: 60),
-            imageView.heightAnchor.constraint(equalToConstant: 60),
-            
-            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -90),
-            button.heightAnchor.constraint(equalToConstant: 48)
-        ])
-    }
-    
-    @objc private func showWebView() {
-        performSegue(withIdentifier: "ShowWebView", sender: self)
     }
 }
