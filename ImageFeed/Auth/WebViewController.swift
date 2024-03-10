29706f9f5bc42c69579d89b9dc185fbf78b6e543
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-03-11 02:08:56 +0300

    Added loading login page

--- ImageFeed/Auth/WebViewController.swift
+++ ImageFeed/Auth/WebViewController.swift
@@ -11,4 +11,35 @@ import WebKit
 final class WebViewController: UIViewController {
     @IBOutlet weak var webView: WKWebView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        webView.navigationDelegate = self
+        loadAuthView()
+    }
+    
+    private func loadAuthView() {
+        guard var urlComponents = URLComponents(string: Constants.unsplashAuthorizeURLString) else {
+            print("urlComponents is faild")
+            return
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope)
+        ]
+        guard let url = urlComponents.url else {
+            print("url is failed")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
+
+extension WebViewController: WKNavigationDelegate {
+    
 }
--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -10,8 +10,10 @@ import Foundation
 enum Constants {
     static let accessKey = "KEofjj-QLCo5Ux6HcfLoc21tc6hbJbSfy93fv-1WaQ8"
     static let secretKey = "FDV9nQuHHqeQsxxWD38iQ7fEdk8cLxYP6Sf60HB6Q6w"
-    static let redirectURL = "urn:ietf:wg:oauth:2.0:oob"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     
     static let accessScope = "public+read_user+write_likes"
     static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
 }
