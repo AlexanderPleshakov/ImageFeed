7d81e3418f91e1c8dd23a0bb7ea0b538f1eb05c1
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-03-10 20:24:53 +0300

    Added constants

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Александр Плешаков on 10.03.2024.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "KEofjj-QLCo5Ux6HcfLoc21tc6hbJbSfy93fv-1WaQ8"
+    static let secretKey = "FDV9nQuHHqeQsxxWD38iQ7fEdk8cLxYP6Sf60HB6Q6w"
+    static let redirectURL = "urn:ietf:wg:oauth:2.0:oob"
+    
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
