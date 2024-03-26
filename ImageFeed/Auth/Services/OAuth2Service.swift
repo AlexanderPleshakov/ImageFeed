47987a5aa8b7f979f893a65eac77e1eedf89ddc5
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-03-27 00:01:59 +0300

    Make request method was changed

--- ImageFeed/Auth/Services/OAuth2Service.swift
+++ ImageFeed/Auth/Services/OAuth2Service.swift
@@ -14,24 +14,34 @@ final class OAuth2Service {
     
     private enum NetworkError: Error {
         case codeError
+        case createRequestError
     }
     
-    func makeOAuthTokenRequest(code: String) -> URLRequest {
+    func makeOAuthTokenRequest(code: String) -> URLRequest? {
         let baseURL = URL(string: "https://unsplash.com")!
-         let url = URL(string: "/oauth/token"
-             + "?client_id=\(Constants.accessKey)"         
+        guard
+            let url = URL(string: "/oauth/token"
+             + "?client_id=\(Constants.accessKey)"
              + "&&client_secret=\(Constants.secretKey)"
              + "&&redirect_uri=\(Constants.redirectURI)"
              + "&&code=\(code)"
              + "&&grant_type=authorization_code",
-             relativeTo: baseURL)!
-         var request = URLRequest(url: url)
-         request.httpMethod = "POST"
-         return request
+             relativeTo: baseURL)
+        else {
+            assertionFailure("Failed to create url")
+            return nil
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        return request
     }
     
     func fetchOAuthToken(code: String, handler: @escaping (Result<String, Error>) -> Void) {
         let request = makeOAuthTokenRequest(code: code)
+        guard let request = request else {
+            handler(.failure(NetworkError.createRequestError))
+            return
+        }
         
         let task = URLSession.shared.data(for: request) { result in
             switch result {
