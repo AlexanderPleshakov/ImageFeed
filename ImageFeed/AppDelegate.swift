ec4176cf16b590e085b40759439520c4e5613053
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-03-27 00:27:09 +0300

    Added cleaning app data for debug

--- ImageFeed/AppDelegate.swift
+++ ImageFeed/AppDelegate.swift
@@ -10,27 +10,46 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        clearAppData()
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
+
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    func clearAppData() {
+        UserDefaults.standard.removeObject(forKey: Constants.UserDefaults.bearerTokenKey)
+        // Определение путей кеша и временной директории
+        if let cacheDirectory = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first,
+           let tmpDirectory = FileManager.default.temporaryDirectory.path as? String {
+            
+            // Удаление файлов из кеша
+            do {
+                let cacheURLs = try FileManager.default.contentsOfDirectory(at: cacheDirectory, includingPropertiesForKeys: nil, options: [])
+                for fileURL in cacheURLs {
+                    try FileManager.default.removeItem(at: fileURL)
+                }
+            } catch {
+                print("Ошибка при удалении файлов из кеша: \(error.localizedDescription)")
+            }
+            
+            // Удаление файлов из временной директории
+            do {
+                let tmpURLs = try FileManager.default.contentsOfDirectory(atPath: tmpDirectory)
+                for file in tmpURLs {
+                    let filePath = (tmpDirectory as NSString).appendingPathComponent(file)
+                    try FileManager.default.removeItem(atPath: filePath)
+                }
+            } catch {
+                print("Ошибка при удалении файлов из временной директории: \(error.localizedDescription)")
+            }
+        }
     }
-
 
 }
 
