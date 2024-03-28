9189921b5cc06e106f98d5007a517f8d0d966789
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-03-28 14:16:45 +0300

    Race condition in fetching user profile data fixed

--- ImageFeed/Profile/Services/ProfileService.swift
+++ ImageFeed/Profile/Services/ProfileService.swift
@@ -10,21 +10,35 @@ import Foundation
 final class ProfileService {
     
     private let tokenStorage = OAuth2TokenStorage()
+    private var lastToken: String?
+    private var task: URLSessionTask?
     
     private enum ProfileFetchingError: Error {
-        case incorrectURL, requestCreateFailure, decodeFailure
+        case incorrectURL, requestCreateFailure, decodeFailure,
+        repeatedRequest
     }
     
     func fetchProfile(bearerToken: String, completion: @escaping (Result<ProfileResult, Error>) -> Void) {
+        assert(Thread.isMainThread)
+        
         guard let url = URL(string: "https://api.unsplash.com/me") else {
             completion(.failure(ProfileFetchingError.incorrectURL))
             return
         }
         
+        guard lastToken != bearerToken else {
+            completion(.failure(ProfileFetchingError.repeatedRequest))
+            return
+        }
+        
+        task?.cancel()
+        lastToken = bearerToken
+        
         var request = URLRequest(url: url)
         request.setValue("Bearer \(tokenStorage.token)", forHTTPHeaderField: "Authorization")
         
-        let task = URLSession.shared.data(for: request) { result in
+        let task = URLSession.shared.data(for: request) { [weak self] result in
+            guard let self = self else { return }
             
             switch result {
             case .success(let data):
@@ -39,7 +53,11 @@ final class ProfileService {
             case .failure(let error):
                 completion(.failure(error))
             }
+            
+            self.task = nil
+            self.lastToken = nil
         }
+        self.task = task
         task.resume()
     }
 }
