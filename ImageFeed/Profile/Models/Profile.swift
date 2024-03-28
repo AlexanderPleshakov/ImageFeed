e1448badf2a7c6d760d0d80d6b6d29bfbe07d1d5
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-03-28 13:25:42 +0300

    Added profile model

---
+++ ImageFeed/Profile/Models/Profile.swift
@@ -0,0 +1,23 @@
+//
+//  Profile.swift
+//  ImageFeed
+//
+//  Created by Александр Плешаков on 28.03.2024.
+//
+
+import Foundation
+
+struct Profile {
+    
+    let name: String
+    let username: String
+    let loginName: String
+    let bio: String
+    
+    init(profileResult: ProfileResult) {
+        name = profileResult.firstName + (profileResult.lastName ?? "")
+        username = profileResult.username
+        loginName = "@\(profileResult.username)"
+        bio = profileResult.bio ?? ""
+    }
+}
