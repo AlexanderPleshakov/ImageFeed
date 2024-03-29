8ca35ee2a1452e947e75c2fb487caf766ee31069
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-03-29 20:52:09 +0300

    Added decoder into extension for URLSession

--- ImageFeed/Helpers/URLSession+data.swift
+++ ImageFeed/Helpers/URLSession+data.swift
@@ -11,23 +11,31 @@ enum NetworkError: Error {
     case httpStatusCode(Int)
     case urlRequestError(Error)
     case urlSessionError
+    case decoderError
 }
 
 extension URLSession {
-    func data(
+    func data<T: Decodable>(
         for request: URLRequest,
-        completion: @escaping (Result<Data, Error>) -> Void
+        completion: @escaping (Result<T, Error>) -> Void
     ) -> URLSessionTask {
-        let fulfillCompletionOnTheMainThread: (Result<Data, Error>) -> Void = { result in
+        let fulfillCompletionOnTheMainThread: (Result<T, Error>) -> Void = { result in
             DispatchQueue.main.async {
                 completion(result)
             }
         }
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
         
         let task = dataTask(with: request, completionHandler: { data, response, error in
             if let data = data, let response = response, let statusCode = (response as? HTTPURLResponse)?.statusCode {
                 if 200 ..< 300 ~= statusCode {
-                    fulfillCompletionOnTheMainThread(.success(data))
+                    do {
+                        let resultData = try decoder.decode(T.self, from: data)
+                        fulfillCompletionOnTheMainThread(.success(resultData))
+                    } catch {
+                        fulfillCompletionOnTheMainThread(.failure(NetworkError.decoderError))
+                    }
                 } else {
                     print("Unexpected status code")
                     print(String(data: data, encoding: .utf8) ?? "")
