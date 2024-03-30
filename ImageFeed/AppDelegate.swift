42bfa2722adcb869282ce1408406143788d74dd6
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-03-30 20:37:33 +0300

    Root view controller was created using code

--- ImageFeed/AppDelegate.swift
+++ ImageFeed/AppDelegate.swift
@@ -17,38 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let sceneConfiguration = UISceneConfiguration(
+            name: "Main",
+            sessionRole: connectingSceneSession.role
+        )
+        sceneConfiguration.delegateClass = SceneDelegate.self
 
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        return sceneConfiguration
     }
-
-    func clearAppData() {
-        UserDefaults.standard.removeObject(forKey: Constants.UserDefaults.bearerTokenKey)
-        // Определение путей кеша и временной директории
-        if let cacheDirectory = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first,
-           let tmpDirectory = FileManager.default.temporaryDirectory.path as? String {
-            
-            // Удаление файлов из кеша
-            do {
-                let cacheURLs = try FileManager.default.contentsOfDirectory(at: cacheDirectory, includingPropertiesForKeys: nil, options: [])
-                for fileURL in cacheURLs {
-                    try FileManager.default.removeItem(at: fileURL)
-                }
-            } catch {
-                print("Ошибка при удалении файлов из кеша: \(error.localizedDescription)")
-            }
-            
-            // Удаление файлов из временной директории
-            do {
-                let tmpURLs = try FileManager.default.contentsOfDirectory(atPath: tmpDirectory)
-                for file in tmpURLs {
-                    let filePath = (tmpDirectory as NSString).appendingPathComponent(file)
-                    try FileManager.default.removeItem(atPath: filePath)
-                }
-            } catch {
-                print("Ошибка при удалении файлов из временной директории: \(error.localizedDescription)")
-            }
-        }
-    }
-
 }
 
