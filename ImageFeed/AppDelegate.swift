9355d0b9d851532b40fd0c397076be1405955501
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-04-08 21:41:15 +0300

    Added customization for progress hud,

--- ImageFeed/AppDelegate.swift
+++ ImageFeed/AppDelegate.swift
@@ -6,11 +6,17 @@
 //
 
 import UIKit
+import ProgressHUD
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        ProgressHUD.colorHUD = UIColor(red: 255, green: 255, blue: 255, alpha: 0.5)
+        ProgressHUD.colorAnimation = UIColor(named: "YP Black") ?? .lightGray
+        ProgressHUD.animationType = .activityIndicator
+        ProgressHUD.mediaSize = 51
+        ProgressHUD.marginSize = 13
         return true
     }
 
