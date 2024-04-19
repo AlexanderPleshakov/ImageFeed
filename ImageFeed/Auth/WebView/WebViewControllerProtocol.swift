4b87ed6dd2444fcb46927f96f052e903219ec9b8
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-04-19 17:39:50 +0300

    Added protocol for web view

---
+++ ImageFeed/Auth/WebView/WebViewControllerProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  WebViewControllerProtocol.swift
+//  ImageFeed
+//
+//  Created by Александр Плешаков on 19.04.2024.
+//
+
+import Foundation
+
+protocol WebViewControllerProtocol {
+    var presenter: WebViewPresenterProtocol? {get set}
+}
