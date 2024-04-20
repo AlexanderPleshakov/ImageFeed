faa99b174b3e01d0aed7af13fa52295fb6958997
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-04-20 23:41:50 +0300

    Added web view test file

---
+++ ImageFeedTests/WebViewTests.swift
@@ -0,0 +1,13 @@
+//
+//  WebViewTests.swift
+//  WebViewTests
+//
+//  Created by Александр Плешаков on 20.04.2024.
+//
+
+import XCTest
+@testable import ImageFeed
+
+final class WebViewTests: XCTestCase {
+
+}
