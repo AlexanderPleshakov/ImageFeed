b29dd359c40861fc5018a12f6493cd05d3ab7dc2
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-04-21 13:44:02 +0300

    Added tests for images list

---
+++ ImageFeedTests/ImagesListTests/ImagesListTests.swift
@@ -0,0 +1,15 @@
+//
+//  ImagesListTests.swift
+//  ImageFeedTests
+//
+//  Created by Александр Плешаков on 21.04.2024.
+//
+
+import XCTest
+@testable import ImageFeed
+
+final class ImagesListTests: XCTestCase {
+    func someTest() {
+        
+    }
+}
