a55741aea01d0ccaee944831feb6a6cf5fd4195c
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-04-21 14:21:31 +0300

    Protocol for images list presenter was implemented

--- ImageFeed/ImagesList/Protocols/ImagesListPresenterProtocol.swift
+++ ImageFeed/ImagesList/Protocols/ImagesListPresenterProtocol.swift
@@ -8,5 +8,11 @@
 import Foundation
 
 protocol ImagesListPresenterProtocol {
-    
+    var view: ImagesListViewControllerProtocol? { get set }
+    func photosCount() -> Int
+    func getPhoto(at index: Int) -> Photo
+    func updatePhotos()
+    func nextPage()
+    func shouldGetNextPage(for index: Int)
+    func changeLike(at index: Int, _ completion: @escaping (Result<Bool, Error>) -> Void)
 }
