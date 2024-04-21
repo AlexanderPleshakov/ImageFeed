082cd8a87c22b16efb5113cc7be038f986c159e5
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-04-21 14:31:17 +0300

    Added methods for working with photos in list presenter
    Update photos method of presenter protocol was changed

--- ImageFeed/ImagesList/ImagesListPresenter.swift
+++ ImageFeed/ImagesList/ImagesListPresenter.swift
@@ -7,6 +7,35 @@
 
 import Foundation
 
-final class ImagesListPresenter {
+final class ImagesListPresenter: ImagesListPresenterProtocol {
+    weak var view: ImagesListViewControllerProtocol?
+    private let imagesListService = ImagesListService.shared
+    private var photos = [Photo]()
     
+    func photosCount() -> Int {
+        photos.count
+    }
+    
+    func getPhoto(at index: Int) -> Photo {
+        photos[index]
+    }
+    
+    func updatePhotosAndGetCounts() -> (Int, Int) {
+        let oldCount = photosCount()
+        photos = imagesListService.photos
+        let newCount = photosCount()
+        return (oldCount, newCount)
+    }
+    
+    func nextPage() {
+        
+    }
+    
+    func shouldGetNextPage(for index: Int) {
+        <#code#>
+    }
+    
+    func changeLike(at index: Int, _ completion: @escaping (Result<Bool, any Error>) -> Void) {
+        <#code#>
+    }
 }
--- ImageFeed/ImagesList/Protocols/ImagesListPresenterProtocol.swift
+++ ImageFeed/ImagesList/Protocols/ImagesListPresenterProtocol.swift
@@ -11,7 +11,7 @@ protocol ImagesListPresenterProtocol {
     var view: ImagesListViewControllerProtocol? { get set }
     func photosCount() -> Int
     func getPhoto(at index: Int) -> Photo
-    func updatePhotos()
+    func updatePhotosAndGetCounts() -> (Int, Int)
     func nextPage()
     func shouldGetNextPage(for index: Int)
     func changeLike(at index: Int, _ completion: @escaping (Result<Bool, Error>) -> Void)
