9f0528095c22a439da990db3ba5f76d2f9509910
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-04-21 13:43:00 +0300

    Added protocols for images list presenter and controller

---
+++ ImageFeed/ImagesList/Protocols/ImagesListPresenterProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListPresenterProtocol.swift
+//  ImageFeed
+//
+//  Created by Александр Плешаков on 21.04.2024.
+//
+
+import Foundation
+
+protocol ImagesListPresenterProtocol {
+    
+}
---
+++ ImageFeed/ImagesList/Protocols/ImagesListViewControllerProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListViewControllerProtocol.swift
+//  ImageFeed
+//
+//  Created by Александр Плешаков on 21.04.2024.
+//
+
+import Foundation
+
+protocol ImagesListViewControllerProtocol: NSObject {
+    
+}
