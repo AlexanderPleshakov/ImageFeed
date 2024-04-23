dcd49f16d35f59225611e55193946c8365a187cd
AlexanderPleshakov <sanya.pleshakov.95@mail.ru>
2024-04-23 22:04:57 +0300

    Added profile tests

---
+++ ImageFeedTests/ProfileTests/ProfileTests.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileTests.swift
+//  ImageFeedTests
+//
+//  Created by Александр Плешаков on 23.04.2024.
+//
+
+import ImageFeed
+import XCTest
+
+final class ProfileTests: XCTestCase {
+    
+}
